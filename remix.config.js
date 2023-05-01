818059b23676e2ac07c65e06d9b0b488514a6093
bharathkumar1807 <bharathkumar1807@gmail.com>
2023-05-01 16:35:28 -0500

    Add files via upload

---
+++ remix.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+	cacheDirectory: './node_modules/.cache/remix',
+	future: {
+		v2_errorBoundary: false,
+		v2_meta: false,
+		v2_normalizeFormMethod: false,
+		v2_routeConvention: false,
+	},
+	ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
+}
---
+++ site.config.ts
@@ -0,0 +1,15 @@
+const siteConfig = {
+	appName: 'eKindle Application',
+	noOfDaysToReturn: 7,
+	publicLinks: [{name: 'Library', href: '/library'}],
+	navigationLinks: [
+		{name: 'Home', href: '/'},
+		{name: 'Payment History', href: '/payment-history'},
+	],
+	adminLinks: [
+		{name: 'Users', href: '/admin'},
+		{name: 'Media', href: '/admin/media'},
+	],
+}
+
+export default siteConfig
